d28b4214231b536ec8febb76e3d7072d9eef022e
Syahrul Bastomy <syahrulb60@gmail.com>
2022-09-29 17:20:20 +0700

    20 september 2022 : - penambahan image. - penambahan bg - penambahan fitur address

--- src/component/Address.jsx
+++ src/component/Address.jsx
@@ -1,6 +1,12 @@
 import Grid from '@mui/material/Grid';
 import addressLogo from '../assets/image/address-logo.png'
 import Typography from '@mui/material/Typography';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import styles from './address.module.css';
+import PhoneIcon from '@mui/icons-material/Phone';
+import StarIcon from '@mui/icons-material/Star';
+import Box from '@mui/material/Box';
+import StarHalfIcon from '@mui/icons-material/StarHalf';
 function Address() {
     return <>
         <Grid container  sx={{
@@ -17,11 +23,57 @@ function Address() {
                 alignItems: 'flex-start',
                 flexDirection: 'column',
             }}>
-                <Typography variant="h5" gutterBottom>
+                <Typography variant="h5" gutterBottom sx={{
+                    mb:2
+                }}>
                     Pan Pacific Sonargaon/ Dhaka,BD
                 </Typography>
+                <Typography variant="subtitle2" sx={{alignItems:"center"}}  gutterBottom>
+                    <span className={styles.spanx} >
+                        <LocationOnIcon sx={{bgcolor:'grey.200',mr:1 }} fontSize="small" /> 
+                        107 Kazi Nazrul Islam Avenue, Dhaka 1215, Bangladesh                   
+                    </span>
+                    <br />
+                    <span className={styles.spanx}>
+                        <PhoneIcon sx={{bgcolor:'grey.200', mr:1 }} fontSize="small" />
+                        Panpacific.Com: 550-208-00808
+                    </span>
+                </Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={4} sx={{
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: 'column'
+                }}>
+                <p className={styles.p}>
+                    <span className={styles.ratingBig}>
+                        4.5
+                    </span>
+                    <span>
+                        /5
+                    </span>
+                </p>
+                <Box sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexDirection: 'row'
+                }}>
+                    <StarIcon sx={{
+                        color:'orange'
+                    }}/>
+                    <StarIcon sx={{
+                        color:'orange'
+                    }}/>
+                    <StarIcon sx={{
+                        color:'orange'
+                    }}/>
+                    <StarIcon sx={{
+                        color:'orange'
+                    }}/>
+                    <StarHalfIcon sx={{
+                        color:'orange'
+                    }}/>
+                </Box>
             </Grid>
         </Grid>
     </>;
--- src/pages/HalamanUtama.jsx
+++ src/pages/HalamanUtama.jsx
@@ -1,12 +1,13 @@
 import Header from '../component/Header.jsx';
 import Address from '../component/Address.jsx';
 import Container from '@mui/material/Container';
-
+import bg from '../assets/image/bg.png';
 function HalamanUtama() {
     return <>
         <Header />
         <Container>
             <Address />
+            <img src={bg} width="100%" alt="baggian foto" />
         </Container>
     </>;
 }
